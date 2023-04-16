7247181486b7b96cdee7e7fd867ff089388c8812
Vaishnavi Desai <78345767+Vaishnavi-Desai-0204@users.noreply.github.com>
2023-04-15 19:03:58 -0700

    app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template, redirect, url_for
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    filename = file.filename
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return redirect(url_for('inventory'))
+
+@app.route('/inventory')
+def inventory():
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('inventory.html', files=files)
+
+if __name__ == '__main__':
+    app.run(debug=True)
